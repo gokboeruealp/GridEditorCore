7b46bd94bf9fcd12fccc746ab033a443f7d23397
Berk Örenbaşı <berkorenbasi@icloud.com>
2023-09-23 14:16:13 +0300

    Update Tile.cs

--- Assets/Scripts/GridEditorCore/Model/Tile.cs
+++ Assets/Scripts/GridEditorCore/Model/Tile.cs
@@ -26,32 +26,5 @@ namespace Borboerue
         {
             TileObject = tileObject;
         }
-
-        public void Init(ETileType type, Vector3 position, GameObject tileObject)
-        {
-            Type = type;
-            CreateTile(position, tileObject);
-        }
-
-        public void CreateTile(Vector3 position, GameObject tileObject)
-        {
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-            var mf = gameObject.AddComponent<MeshFilter>();
-            gameObject.AddComponent<MeshRenderer>();
-            gameObject.AddComponent<BoxCollider>();
-
-            mf.mesh = cube.GetComponent<MeshFilter>().mesh;
-            Destroy(cube);
-
-            gameObject.transform.position = position;
-            gameObject.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
-            gameObject.transform.parent = tileObject.transform;
-
-            TileObject = tileObject;
-
-            GetTileObject().GetComponent<Tile>().SetTileType(Type);
-            GetTileObject().GetComponent<Tile>().SetTileObject(TileObject);
-        }
     }
 }
